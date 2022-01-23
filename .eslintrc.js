bb6137f0b1b5822d436521ae65666434b04d2082
rabiulhasan3 <rabiul.fci@gmail.com>
2022-01-23 23:32:45 +0600

    create weather application

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  overrides: [
+    {
+      files: ['src/views/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
+    },
+  ],
+  // add your custom rules here
+  rules: {},
+}
---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main>
+     <v-toolbar color="indigo">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Title</v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-btn text>
+          <nuxt-link to="/">Weather Application</nuxt-link>
+        </v-btn>
+          </v-toolbar>
+          <transition>
+            <nuxt />
+          </transition>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+
+<style>
+a{
+  text-decoration: none;
+  color: '#FFF'
+}
+</style>
